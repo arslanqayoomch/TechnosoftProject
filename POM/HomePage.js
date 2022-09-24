3ee8b68b036915decb35352154dd59a310effacd
Arslan Chaudhry <aqayoomch@gmail.com>
2022-09-24 17:00:31 -0400

    project

---
+++ POM/HomePage.js
@@ -0,0 +1,210 @@
+const Commands = require("../Commands")
+
+
+class Homepage{
+    
+    commands = new Commands();
+
+    emailField = "#loginFormEmailInput"
+    passwordField = "#loginFormPasswordInput"
+    signinBtn = "#loginFormSubmitButton"
+    errMsg = "//h3[contains(text(),'Email and password')]"
+    body = "#app"
+
+    signUpLink = "//a[contains(text(),'Create')]"
+    feedbackLink = "//a[text()='Feedback']"
+    signInDropdown = "//button[text()='Sign in']"
+
+    
+
+    travelersBtn = "//label[text()='Travelers']/following-sibling::input/following-sibling::button"
+    addAdultsBtn = "//input[@aria-label='Adults ']/following-sibling::button/span"
+    numOfAdults = "//input[@aria-label='Adults ']/following-sibling::button"
+    
+    addChildrenBtn = "//input[contains(@aria-label,'Children')]/following-sibling::button"
+    removeChildrenBtn = "//input[contains(@aria-label,'Children')]/preceding-sibling::button";
+    numOfChildren = "//input[contains(@aria-label,'Children')]"
+
+    child1Dropdown = "//label[contains(text(),'Child 1')]/following-sibling::select"
+    child2Dropdown = "//label[contains(text(),'Child 2')]/following-sibling::select"
+    child3Dropdown = "//label[contains(text(),'Child 3')]/following-sibling::select"
+
+    doneBtn = "#traveler_selector_done_button"
+
+    numOfTravelers = "//button[contains(text(),'travelers')]"
+
+    signInBtn = "//button[text()='Sign in']";
+    signInLink = "//a[text()='Sign in']";
+    numOfDropdownsForChildren = "//select[contains(@name,'child-traveler_selector')]"
+    
+    async childrenAgeDropdownisDisplayed(){
+        return await this.commands.isWebElementDisplayed(this.numOfDropdownsForChildren);
+    }
+    async addChildrenBtnisEnabled(){
+        return await this.commands.isWebElementEnabled(this.addChildrenBtn);
+    }
+    async removeChildrenBtnisEnabled(){
+        return await this.commands.isWebElementEnabled(this.removeChildrenBtn);
+    }
+    async verify2Dropdown(){
+        const locators = await this.commands.findWebElements(this.numOfDropdownsForChildren);
+        if(locators.length===2){
+            return true;    
+        }
+    }
+    async verify6Dropdown(){
+        const locators = await this.commands.findWebElements(this.numOfDropdownsForChildren);
+        if(locators.length===6){
+            return true;    
+        }
+    }
+    async verify5Dropdown(){
+        const locators = await this.commands.findWebElements(this.numOfDropdownsForChildren);
+        if(locators.length===5){
+            return true;    
+        }
+    }
+    async verify0Dropdown(){
+        const locators = await this.commands.findWebElements(this.numOfDropdownsForChildren);
+        if(locators.length===0){
+            return true;    
+        }
+    }
+    async clickOnTravelers(){
+        await this.commands.clickWebElement(this.travelersBtn);
+    }
+    async setNumOfAdults(num){ 
+        for(let i=2; i<num ; i++){
+            await this.commands.clickWebElement(this.addAdultsBtn);
+            const numOfAdults = await this.commands.getTextFromWebElement(this.numOfAdults);
+            if(numOfAdults.localeCompare(num)===0){
+                break;
+            }
+        }
+    }
+    async setNumOfChildren(num){
+        for(let i=0; i<num ; i++){
+            await this.commands.clickWebElement(this.addChildrenBtn);
+            const numOfChildren = await this.commands.getTextFromWebElement(this.numOfChildren);
+            if(numOfChildren.localeCompare(num)===0){
+                break;
+            }
+        }
+    }
+    async clickMinusButton(num){
+        for(let i=0; i<num;i++){
+            await this.commands.clickWebElement(this.removeChildrenBtn);
+        }
+    }
+    async selectChildOneAge(num){
+        await this.commands.selectFromDropdown(this.child1Dropdown, num);
+    }
+    async selectChildTwoAge(num){
+        await this.commands.selectFromDropdown(this.child2Dropdown, num);
+    }
+    async selectChildThreeAge(num){
+        await this.commands.selectFromDropdown(this.child3Dropdown, num);
+    }
+    async clickDone(){
+        await this.commands.clickWebElement(this.doneBtn);
+    }
+    async verifyTravelersNumEqualInputof(num){
+        const text = await this.commands.getTextFromWebElement(this.numOfTravelers);
+        if (text.includes(num)){
+            return true;
+        }
+    }
+    async clickSignInBtn(){
+        await this.commands.clickWebElement(this.signInBtn);
+        await this.commands.clickWebElement(this.signInLink);
+    }
+ 
+    commands = new Commands();
+    
+    signUpemail = '#signupFormEmailInput'
+    firstname = '#signupFormFirstNameInput'
+    lastname = '#signupFormLastNameInput'
+    signUppassword = '#signupFormPasswordInput'
+    
+    errMsgValidEmail = "//div[contains(text(),'Enter a valid')]"
+    errMsgFirstname = "//div[contains(text(),'First name cannot')]"
+    errMsgLastname = "//div[contains(text(),'Last name cannot')]"
+    signUpFormCheckbox = "//input[@type='checkbox']";
+    continueBtn = "#signupFormSubmitButton"
+    checkbox = "//label[contains(text(),'Keep me signed ')]"
+    termsAndConditionsLink = "//a[contains(text(),'Terms and')]"
+
+    async enterSignUpEmail(value){
+        await this.commands.clickWebElement(this.signUpemail);
+        await this.commands.typeInWebElement(this.signUpemail, value);
+    }
+    async enterFirstName(value){
+        await this.commands.clickWebElement(this.firstname);
+        await this.commands.typeInWebElement(this.firstname, value);
+    }
+    async enterLastName(value){
+        await this.commands.clickWebElement(this.lastname);
+        await this.commands.typeInWebElement(this.lastname, value);
+    }
+    async enterSignUpPassword(value){
+        await this.commands.clickWebElement(this.signUppassword);
+        await this.commands.typeInWebElement(this.signUppassword, value);
+    }
+    async verifySignUpEmailErrMsg(){
+        await this.commands.isWebElementDisplayed(this.errMsgValidEmail);
+    }
+    async verifyFirstnameErrMsg(){
+        await this.commands.isWebElementDisplayed(this.errMsgFirstname);
+    }
+    async verifyLastnameErrMsg(){
+        await this.commands.isWebElementDisplayed(this.errMsgLastname);
+    }
+    async checkboxDisplayed(){
+        await this.commands.scrollAndFindWebElement(this.checkbox);
+        // const text = await this.commands.getTextFromWebElement(this.checkbox);
+        return await this.commands.isWebElementDisplayed(this.checkbox);
+    }
+    async checkboxEnabled(){
+        return await this.commands.isWebElementEnabled(this.signUpFormCheckbox);
+    }
+    async continueBtnDisplayed(){
+        await this.commands.scrollAndFindWebElement(this.continueBtn);
+        return await this.commands.isWebElementDisplayed(this.continueBtn);
+    }
+    async continueBtnEnabled(){
+        return await this.commands.isWebElementEnabled(this.continueBtn);
+    }
+    async clickTermsAndConditons(){
+        await this.commands.clickWebElement(this.termsAndConditionsLink);
+    }
+
+  
+
+ async enterLoginEmail(value){
+        await this.commands.clickWebElement(this.emailField);
+        await this.commands.typeInWebElement(this.emailField, value);
+    }
+    async enterPassword(value){
+        await this.commands.clickWebElement(this.passwordField);
+        await this.commands.typeInWebElement(this.passwordField, value);
+    }
+    async clickSignInBtn(){
+        await this.commands.clickWebElement(this.signinBtn);
+    }
+    async errMsgisDisplayed(){
+        return await this.commands.isWebElementDisplayed(this.errMsg);
+    }
+    async clickSignUpLink(){
+        await this.commands.clickWebElement(this.signUpLink);
+    }
+    async clickFeedbackLink(){
+        await this.commands.clickWebElement(this.feedbackLink);
+    }
+    async clickSignInDropdown(){
+        await this.commands.clickWebElement(this.signInDropdown);
+    }
+
+
+}
+
+module.exports=Homepage
---
+++ features/Hotels/hotel.feature
@@ -0,0 +1,96 @@
+Feature: Project 1
+
+    @report1
+    Scenario: TC-18 Verify user can update number of guests on Homepage
+        Given I am on hotels HomePage
+        When I click on Travelers
+        Then I select adults as 6
+        Then I select children as 3
+        Then I select first child age as 4
+        Then I select second child age under 1
+        Then I select third child age 7
+        When I click Done
+        Then I verify total number of guests in sum of adults and children same as selected on step 3 and 4
+
+    @report2
+    Scenario: TC-21 Verify verification message for invalid sign in credentials
+        Given I am on hotels HomePage
+        When I click on Sign in link
+        And I enter username as email
+        And I enter password as password
+        When I click on Sign In button
+        Then I verify verification message is displayed
+        
+    
+    @report3
+    Scenario: TC-22 Verify error message for invalid data in SignUp form
+        Given I am on hotels Homepage
+        When I click on "Sign in" link
+        When I click on SignUp link
+        When I enter "<signupemail>" as invalid email
+        When I enter "<firstname>" as invalid firstname
+        When I enter "<lastname>" as invalid lastname
+        When I enter "<signuppassword>" as invalid password
+        Then I verify "Enter a valid email address" error is displayed
+        Then I verify "First name cannot contain special characters" error is displayed
+        Then I verify "Last name cannot contain special characters" error is displayed
+        Then I verify "Keep me signed in" checkbox is displayed and enabled
+        Then I verify "Continue" button is displayed but NOT enabled
+        Examples:
+            | signupemail | firstname | lastname |  signuppassword    |
+            |  #!@### | !@  | %^&  | 123$wg1 |
+        
+    @report4
+    Scenario: TC-20 Verify TermsAndConditions link and PrivacyStatements link open correct page on new tab
+        Given I am on hotels HomePage
+        When I click on "Sign in" link
+        When I click on SignUp link
+        When I click on “Terms and Conditions” link
+        Then I verify “Terms and Conditions” page opens in new tab
+        When I click “Privacy Statement” link
+        Then I verify “Privacy Statement” page opens in new tab
+
+    @report5
+    Scenario: TC-24 Verify error is displayed when user submits the empty feedback form
+        Given I am on hotels HomePage
+        When I click on "Sign in"
+        When I click on "Feedback"
+        When I click on Submit button
+        Then I verify error message is displayed "Please fill in the required information highlighted below."
+        Then I verify star boxes section is in a red dotted box
+
+    @report6
+    Scenario: TC-25 Verify user can submit feedback after completing the feedback form
+        Given I am on hotels HomePage
+        When I click on "Sign in"
+        When I click on "Feedback"
+        When I select a star-rating
+        When I enter any comments
+        When I select any option for “How likely are you to return to Hotels.com”
+        When I select any answer for “Prior to this visit, have you ever booked on Hotels.com”
+        When I select any answer for ”Did you accomplish what you wanted to do on this page”
+        When I click on Submit button
+        Then I verify “THANK YOU FOR YOUR FEEDBACK.“ is displayed
+        
+     @report7
+    Scenario: TC-28 Verify Child-age dropdowns are same as number of Children selected
+        Given I am on hotels HomePage
+        When I click on Travelers
+        When I select "children" as 2
+        When I verify Children-age dropdown are 2
+        Then I verify Plus-button is enabled
+        Then I verify minus-button is enabled
+        When I select "children" as 6
+        When I verify Children-age dropdown are 6
+        Then I verify Plus-button is disabled
+        Then I verify minus-button is enabled
+        When I select "children" as 5
+        When I verify Children-age dropdown are 5
+        Then I verify Plus-button is enabled
+        Then I verify minus-button is enabled
+        When I select "children" as 0
+        Then I verify Children-age dropdown is NOT Displayed
+        Then I verify Plus-button is enabled
+        Then I verify minus-button is disabled
+
+        
